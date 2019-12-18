369e52fd438d3210591f45d89b24068c77b4594f
LunchBox <Abmaves98@gmail.com>
2019-12-17 18:02:04 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+win.loadURL('https://lutris.net/games/')
+
+}
+
+app.on('ready', createWindow)
